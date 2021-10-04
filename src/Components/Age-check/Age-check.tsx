74d44d3c97b9497fbdf5176a8bd2181ef6c3d316
alvaromartin <alvaro.martin.lorenzo@outlook.com>
2021-10-04 15:48:36 +0200

    Add pages

---
+++ src/Components/Age-check/Age-check.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function AgeCheck() {
+  return (
+    <h1>Welcome to Alvaro´s test</h1>
+  );
+}
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -5,9 +5,9 @@ export default function Header() {
   return (
     <div className="header">
       <div className="header-container">
-        <div className="number">1</div>
-        <div className="number">2</div>
-        <div className="number">3</div>
+        <p className="number">1</p>
+        <p className="number">2</p>
+        <p className="number">3</p>
       </div>
     </div>
   );
