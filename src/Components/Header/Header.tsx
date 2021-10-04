e7984a81dbbd1a2d2cd194a9bafabc2c0d8cb85b
alvaromartin <alvaro.martin.lorenzo@outlook.com>
2021-10-04 15:35:56 +0200

    Add header style

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header-style.scss';
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header-container">
+        <div className="number">1</div>
+        <div className="number">2</div>
+        <div className="number">3</div>
+      </div>
+    </div>
+  );
+}
