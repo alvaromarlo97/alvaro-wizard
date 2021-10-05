fd66147d42c5a00a11f648aee4c7be0d547415f9
alvaromartin <alvaro.martin.lorenzo@outlook.com>
2021-10-05 15:23:34 +0200

    Add clue input

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,12 +1,55 @@
 import React from 'react';
 // import { useDispatch, useSelector } from 'react-redux';
 // import { loginUserData } from '../../redux/actions/login-creators';
+import './Login-style.scss';
 
 export default function Login() {
   // const data = useSelector((store : any) => store?.Age);
-
   return (
-    <h1 className="welcome">Login for trial</h1>
+    <div className="main-container">
+      <h1 className="trial-text">Login for trial</h1>
+      <div className="user-data">
+        <div className="user-name">
+          <h3 className="user-name-text">Crea tu Usuario</h3>
+          <input type="text" className="user-name-input" />
+        </div>
+        <div className="user-password">
+          <div className="new-password">
+            <h3 className="new-password-text">Crea tu Contraseña</h3>
+            <div className="new-password-container">
+              <input type="text" className="new-password-input" />
+              <button type="button" className="new-password-button" />
+            </div>
+          </div>
+          <div className="new-password">
+            <h3 className="new-password-text">repite tu Contraseña</h3>
+            <div className="new-password-container">
+              <input type="text" className="new-password-input" />
+              <button type="button" className="new-password-button" />
+            </div>
+
+          </div>
+        </div>
+        <p className="clue-info">
+          También puedes crear una pista que te ayude a recordar tu contraseña
+        </p>
+        <div className="clue">
+          <div className="clue-text-container">
+            <h3 className="clue-text">Crea tu pista para recordar tu contraseña (opcional)</h3>
+            <div className="clue-text-info">
+              i
+            </div>
+
+          </div>
+          <input type="text" maxLength={60} className="clue-input" id="message" />
+          <div className="clue-input-counter" id="counter">
+            Max60
+          </div>
+
+        </div>
+      </div>
+
+    </div>
 
   );
 }
