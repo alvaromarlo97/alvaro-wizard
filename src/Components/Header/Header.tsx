5a0947921d91b65f5c0d381a9528620501fa0922
alvaromartin <alvaro.martin.lorenzo@outlook.com>
2021-10-06 18:45:20 +0200

    Add header details

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import './Header-style.scss';
 
 export default function Header() {
+  const page: any = useSelector((store : any) => store?.Page);
+  let num : any = 2;
+  if (page === 1) {
+    num = 'x';
+  }
   return (
     <div className="header">
       <div className="header-container">
-        <p className="number">1</p>
-        <p className="number">2</p>
+
+        <p className="number">{num}</p>
+        <p className="number">{num}</p>
         <p className="number">3</p>
       </div>
     </div>
